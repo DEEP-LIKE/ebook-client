063101795a7b77692c5fe1770a5be5f89e3c320a
Juan Ruiz <jr311_@hotmail.com>
2025-09-14 09:40:50 -0600

    Fi: Meta url image

--- activos/fordcoatzacoalcos/lib/functions.php
+++ activos/fordcoatzacoalcos/lib/functions.php
@@ -5,7 +5,7 @@ $dotenv->load();
 
 function read_json($path = './json/ford.json') {
     $json = file_get_contents($path);
-    $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+    $unwanted_array = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y');
 
     if (!$json) {
         $pathArray = explode('/', dirname(__FILE__));
@@ -21,8 +21,6 @@ function read_json($path = './json/ford.json') {
         }
 
         $copyJson = $json;
-
-        // Asegúrate de que las propiedades del título existen antes de usarlas
         $titleData = isset($json['title']) ? $json['title'] : [];
         $headerData = isset($json['header']) ? $json['header'] : [];
 
@@ -42,7 +40,6 @@ function read_json($path = './json/ford.json') {
 
         unset($copyJson['cars']);
         $cars = [];
-        // Asegúrate de que los carros existen antes de iterar
         $jsonCars = isset($json['cars']) ? $json['cars'] : [];
         foreach ($jsonCars as $car) {
             $cleanCarName = strtr( $car['name'], $unwanted_array );
@@ -55,13 +52,22 @@ function read_json($path = './json/ford.json') {
         }
         $copyJson['cars'] = $cars;
 
-        // AÑADE ESTA LÍNEA PARA INCLUIR LA PROPIEDAD site_url
+        // Se agrega la URL del sitio en el JSON, si no existe
         $copyJson['title']['site_url'] = 'https://' . $folderName . '.ebookford.com';
 
         file_put_contents($jsonFilePath, json_encode($copyJson));
         $json = file_get_contents($path);
     }
-    return json_decode($json);
+    $decodedJson = json_decode($json);
+    
+    // Si el JSON se lee desde el archivo y no tiene la propiedad site_url, la añadimos.
+    if (isset($decodedJson->title) && !isset($decodedJson->title->site_url)) {
+        $pathArray = explode('/', dirname(__FILE__));
+        $folderName = $pathArray[count($pathArray)-2];
+        $decodedJson->title->site_url = 'https://' . $folderName . '.ebookford.com';
+    }
+    
+    return $decodedJson;
 }
 
 /******** Sections ********/
@@ -69,30 +75,30 @@ function read_json($path = './json/ford.json') {
 //title
 function get_title() {
     $json = read_json();
-    return isset($json->title) ? $json->title : (object) ['title' => ''];
+    return isset($json->title) ? $json->title : (object) ['title' => '', 'id' => '', 'site_url' => '', 'opengraph' => ''];
 }
 // header
 function get_header() {
     $json = read_json();
     return isset($json->header) ? $json->header : (object) ['title' => '', 'imagen' => ''];
 }
-// asesor
+// asesor 
 function get_asesor() {
     $json = read_json();
     return isset($json->asesor) ? $json->asesor : (object) ['name' => '', 'picture' => '', 'tel' => '', 'whats' => '', 'textw' => '', 'avalible' => false];
 }
-// Cars
+// Cars 
 function get_summary_cars() {
     $json = read_json();
     return isset($json->cars) ? $json->cars : [];
 }
 
-// map
+// map 
 function get_map() {
     $json = read_json();
     return isset($json->map) ? $json->map : '';
 }
-// terms
+// terms 
 function get_terms() {
     $json = read_json();
     return isset($json->terms) ? $json->terms : '';
@@ -114,7 +120,7 @@ function get_face() {
     return isset($json->facebook) ? $json->facebook : '';
 }
 
-// Promotion
+// Promotion 
 function get_summary_promotion() {
     $json = read_json();
     return isset($json->promos) ? $json->promos : [];
@@ -126,7 +132,7 @@ function get_urlmap() {
     return isset($json->urlmap) ? $json->urlmap : '';
 }
 
-// waze
+// waze 
 function get_waze() {
     $json = read_json();
     return isset($json->waze) ? $json->waze : '';
