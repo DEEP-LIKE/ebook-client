e9ad1e7df71230000699ab59745d793f76e15861
Juan Ruiz <jr311_@hotmail.com>
2025-09-14 08:12:41 -0600

    Fix: Add base

--- activos/fordcavsamotors/index.php
+++ activos/fordcavsamotors/index.php
@@ -18,6 +18,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <base href="/activos/<?php echo $title_site->id; ?>/">
   <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
   <title><?php echo $title_site->title; ?></title>
   <!-- Bootstrap CSS -->
--- activos/fordcoatzacoalcos/index.php
+++ activos/fordcoatzacoalcos/index.php
@@ -19,6 +19,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
+  <base href="/activos/<?php echo $title_site->id; ?>/">
   <title><?php echo $title_site->title; ?></title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
